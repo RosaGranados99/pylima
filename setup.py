7e884226a01d2b6ab3b887246f532e328284a325
RosaGranados99 <98921544+RosaGranados99@users.noreply.github.com>
2022-04-27 17:29:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pylima',
       version='0.1',
       description='A Python package for undergraduate math',
-      url='http://github.com/rvf0068/pylima',
-      author='Rafael Villarroel',
-      author_email='rvf0068@gmail.com',
+      url='http://github.com/RosaGranados99/pylima',
+      author='Rosa Granados ',
+      author_email='rosagranados_29@gmail.com',
       license='MIT',
       packages=['pylima'],
       zip_safe=False)
